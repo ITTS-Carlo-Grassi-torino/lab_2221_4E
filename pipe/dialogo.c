3d88e36f5b66f4d1dafd7f4965e17ae1ff1bcd1d
Daniele Ortu <daniele.ortu@itisgrassi.edu.it>
2021-11-23 13:44:30 +0100

    master2

--- pipe/dialogo.c
+++ pipe/dialogo.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
+	int fd1[2],fd2[2];
 	
+	if( pipe(fd1)<0 ){
+		perror("");
+		return 0;
+	}
+	if( pipe(fd2)<0 ){
+		perror("");
+		return 0;
+	}
+	
+	int c_pid;
+	
+	if( (c_pid=fork())==0 ){
+		return 0;
+	} 
 	return 0;
 }
 
